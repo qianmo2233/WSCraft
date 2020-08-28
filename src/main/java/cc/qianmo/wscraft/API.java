297bf2a1f6416ffbac128d0e9122137825e47300
qianmo <672829582qq.com>
2020-08-28 16:11:22 +0800

    修复了非JSON数据可被发送的问题

--- src/main/java/cc/qianmo/wscraft/API.java
+++ src/main/java/cc/qianmo/wscraft/API.java
@@ -1,16 +1,28 @@
 package cc.qianmo.wscraft;
 
 import cc.qianmo.wscraft.WebSocket.connPool;
+import net.sf.json.JSONObject;
 import org.java_websocket.WebSocket;
 
 import java.util.Collection;
 
 public class API {
-    public static void sendMsgToOne(WebSocket conn, String msg) {
-        connPool.sendMessageToUser(conn, msg);
+    public static void sendMsgToOne(String ID, String msg) {
+        try {
+            JSONObject jsonObject = JSONObject.fromObject(msg);
+            WebSocket conn = connPool.getWsByUser(ID);
+            connPool.sendMessageToUser(conn, msg);
+        } catch (Exception e) {
+            Main.getMain().getLogger().warning("数据发送失败，仅支持发送JSON数据");
+        }
     }
     public static void sendMsgToAll(String msg) {
-        connPool.sendMessageToAll(msg);
+        try {
+            JSONObject jsonObject = JSONObject.fromObject(msg);
+            connPool.sendMessageToAll(msg);
+        } catch (Exception e) {
+            Main.getMain().getLogger().warning("数据发送失败，仅支持发送JSON数据");
+        }
     }
     public static Collection<String> getAllOnline() {
         return connPool.getOnlineUser();
